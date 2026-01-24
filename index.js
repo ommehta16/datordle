512ded24d71d958b761121475a04084fff01a114
Om Mehta <ommehta16@icloud.com>
2026-01-24 18:01:12 -0500

    Add basic rendering + re-rendering logic -- no data yet, though!

--- index.js
+++ index.js
@@ -1,19 +1,105 @@
-import {getDataPersist} from "./getdata.js";
-console.log("What the sigma");
+// @ts-check
+
+
+/**
+ * @typedef {{
+ * 	state:string
+ * }} Guess
+ */
+
+/** 
+ * @typedef {{
+ * 	[stat:string]: number
+ * }} StateStats
+ */
+
+/** 
+ * @typedef {{
+ * 	guesses: Guess[],
+ * 	categories: string[],
+ * 	meta: {[key:string]:string},
+ * 	stats: {[state:string]: StateStats}
+ * 	onUpdate: Function
+ * }} Game;
+ */
+
+/** @type {Game} */
+const game = {
+	guesses: [],
+	onUpdate: renderGame,
+	meta: {},
+	categories: [],
+	stats: {}
+};
+
+function loadStats() {
+	// load stats for each state in [somehow]
+}
+
+/** @param {string} category */
+function getPrettyCategory(category) {
+	return category;
+}
+
+/** @param {string[][]} rows */
+function flattenRows(rows) {	
+	const rowList = rows.map((el,i)=> {
+		const rowInner = el.map((inner)=>`<td>${inner}</td>`).join("");
+		return `<tr>${rowInner}</tr>`;
+	});
+	if (rowList.length == 0) return "";
+	let out = `
+	<thead>
+		${rowList[0]}
+	</thead>
+	<tbody>
+		${rowList.slice(1).join("")}
+	</tbody>
+	`;
+	return out;
+}
 
 function renderGame() {
 	const gameElement = document.body.querySelector("#game");
+	if (!gameElement) return;
 
-	let out = "";
+	/** @type {string[][]} */
+	const rows = [];
+	
+	// add header
+	const header = ["State", ...game.categories.map(getPrettyCategory)];
+	rows.push(header);
+	
+	// add previously guessed items as rows
+	for (const guess of game.guesses) {
+		
+		/** @type {string[]} */
+		const rowParts = [];
 
-	let row = "";
-	for (let i=0;i<5;i++) row += `<td data-index=${i}><input /></td>`
-	row = `</tr>${row}</tr>`;
-	for (let i=0;i<5;i++) out += row;
+		rowParts.push(guess.state);
+		
+		if (!(guess.state in game.stats)) throw new Error(`No stats found for state ${guess.state}`);
+		for (const category of game.categories) {
+			const place = game.stats[guess.state][category];
+			rowParts.push(`#${place}`);
+		}
 
-	gameElement.innerHTML = `<tbody>${out}</tbody>`;
+		rows.push(rowParts);
+	}
+	
+	const inputRow = [`<input placeholder="type!"/><button id="sendit"/>`, ...game.categories.map(e=>``)];
+	rows.push(inputRow);
+	
+	let out = flattenRows(rows);
 
-	addListeners();
+	gameElement.innerHTML = out;
+
+	document.querySelector("button#sendit")?.addEventListener("click",e=>{
+		game.guesses.push({
+			state: document.querySelector("input")?.value || "oklahoma"
+		});
+		renderGame();
+	})
 }
 
 renderGame();
